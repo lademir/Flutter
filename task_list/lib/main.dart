d3e3a62cc2aafa1025fa31991981975c9bc2a658
Lademir Junior <lademir.junior@ufv.br>
2021-06-08 18:27:25 -0300

    correcao de push buscador de gifs

--- task_list/lib/main.dart
+++ task_list/lib/main.dart
@@ -47,6 +47,19 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a,b){
+        if(a["ok"] && !b["ok"]) return 1;
+        else if (!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+      _saveData();
+    });
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,10 +94,12 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-              child: ListView.builder(//permite construir a lista conforme vai sendo adicionado coisa nela
-                  padding: EdgeInsets.only(top: 10.0),
-                  itemCount: _toDoList.length,
-                  itemBuilder: buildItem),
+              child: RefreshIndicator(
+                  onRefresh: _refresh,
+                  child: ListView.builder(//permite construir a lista conforme vai sendo adicionado coisa nela
+                      padding: EdgeInsets.only(top: 10.0),
+                      itemCount: _toDoList.length,
+                      itemBuilder: buildItem))
           )
         ],
       ),
@@ -136,6 +151,7 @@ class _HomeState extends State<Home> {
               duration: Duration(seconds: 2),
             );
             // ignore: deprecated_member_use
+            Scaffold.of(context).removeCurrentSnackBar();
             Scaffold.of(context).showSnackBar(snack);
           });
       },
