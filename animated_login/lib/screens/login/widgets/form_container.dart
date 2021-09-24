df509cf7f1eb99e9ce360fbf8fd8e20d74fc6036
Saulo Ferreira de Souza <saulosouza.dev@gmail.com>
2021-09-24 17:32:02 -0300

    animacoes complexas

---
+++ animated_login/lib/screens/login/widgets/form_container.dart
@@ -0,0 +1,25 @@
+import 'package:animated_login/screens/login/widgets/input_field.dart';
+import 'package:flutter/material.dart';
+
+class FormContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Form(
+        child: Column(
+          children: [
+            InputField(
+                icon: Icons.person_outline,
+                hint: "Username",
+                obscure: false),
+            InputField(
+                icon: Icons.lock_outline,
+                hint: "Password",
+                obscure: true),
+          ],
+        ),
+      ),
+    );
+  }
+}
