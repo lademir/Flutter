d2386ff54217d64754c52eefbab99bc636407c2e
Saulo Ferreira de Souza <saulosouza.dev@gmail.com>
2021-09-24 17:44:50 -0300

    finalizando botao

--- animated_login/README.md
+++ animated_login/README.md
@@ -14,3 +14,6 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+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
--- animated_login/lib/screens/login/widgets/stagger_animation.dart
+++ animated_login/lib/screens/login/widgets/stagger_animation.dart
@@ -8,32 +8,49 @@ class StaggerAnimation extends StatelessWidget {
           begin: 320,
           end: 60,
         ).animate(
-            CurvedAnimation(parent: controller, curve: Interval(0.0, 0.150)));
+            CurvedAnimation(parent: controller, curve: Interval(0.0, 0.150))),
+        buttonZoomOut =
+            Tween<double>(begin: 60, end: 1000).animate(CurvedAnimation(
+          parent: controller,
+          curve: Interval(0.5, 1, curve: Curves.bounceOut),
+        ));
 
   final Animation<double> buttonSqueeze;
+  final Animation<double> buttonZoomOut;
 
   Widget _buildAnimation(BuildContext context, Widget child) {
     return Padding(
-      padding: EdgeInsets.only(bottom: 50),
-      child: InkWell(
-        onTap: () {
-          controller.forward();
-        },
-        child: Container(
-          width: buttonSqueeze.value,
-          height: 60,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-              color: Colors.pinkAccent.shade100,
-              borderRadius: BorderRadius.all(Radius.circular(30.0))),
-          child: _buildInside(context),
-        ),
-      ),
-    );
+        padding: EdgeInsets.only(bottom: 50),
+        child: InkWell(
+          onTap: () {
+            controller.forward();
+          },
+          child: buttonZoomOut.value < 70
+              ? Container(
+                  width: buttonSqueeze.value,
+                  height: 60,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.pinkAccent.shade100,
+                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                  child: _buildInside(context),
+                )
+              : Container(
+                  width: buttonZoomOut.value,
+                  height: buttonZoomOut.value,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.pinkAccent.shade100,
+                    shape: buttonZoomOut.value < 500
+                        ? BoxShape.circle
+                        : BoxShape.rectangle,
+                  ),
+                ),
+        ));
   }
 
-  Widget _buildInside(BuildContext context){
-    if(buttonSqueeze.value > 75){
+  Widget _buildInside(BuildContext context) {
+    if (buttonSqueeze.value > 75) {
       return Text(
         "Sign in",
         style: TextStyle(
